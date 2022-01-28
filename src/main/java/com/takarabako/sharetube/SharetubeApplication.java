4570df5f7053b34ce4e4f80313dc2354f9449e23
Hephai Lee <icetime963@gmail.com>
2022-01-28 16:20:19 +0900

    Initial commit from create Spring Boot App

---
+++ src/main/java/com/takarabako/sharetube/SharetubeApplication.java
@@ -0,0 +1,13 @@
+package com.takarabako.sharetube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharetubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharetubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takarabako/sharetube/SharetubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takarabako.sharetube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharetubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

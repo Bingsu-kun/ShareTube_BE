75ceef9601e9a8875ecf5eff4ffe3a0ee04958d3
Hephai Lee <icetime963@gmail.com>
2022-03-05 02:17:55 +0900

    fix: CorsFilter typo

--- src/main/java/com/takarabako/sharetube/auth/oauth2/AuthCorsFilter.java
+++ src/main/java/com/takarabako/sharetube/auth/oauth2/AuthCorsFilter.java
@@ -4,20 +4,16 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.logging.Filter;
-import java.util.logging.LogRecord;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class AuthCorsFilter implements Filter {
 
+  @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
           throws IOException, ServletException
   {
@@ -37,8 +33,4 @@ public class AuthCorsFilter implements Filter {
     }
   }
 
-  @Override
-  public boolean isLoggable(LogRecord record) {
-    return false;
-  }
 }
